05f24e4f8fa96ad3ca9e6fbd8fb2b6672255156a
Jamesie1234 <james.adesanlu@news.co.uk>
2023-03-08 08:03:46 +0000

    first commit

---
+++ deploy-user.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set –e
+
+USER_STACK_NAME="UserManagementStack"
+GROUP_STACK_NAME="UserGroupManagementStack"
+#PROFILE="ss-prod"
+ACCOUNT_ID="548760365095" #"$ACCOUNT_ID" #Replace with account ID if running it locally #$ACCOUNT_ID
+REGION="eu-west-1"   #"$REGION" #Replace with account region if running it locally #$REGION
+
+#Create or update IAM user pipeline
+if ! aws cloudformation describe-stacks --stack-name ${USER_STACK_NAME} --region ${REGION} ; then
+    echo "1"
+    echo "Stack ${main_pipeline_stack} does not exist... Creating the stack..."
+    aws cloudformation create-stack --stack-name ${USER_STACK_NAME} --region ${REGION} --template-body file://infrastructure/user-management/iam-users/aws_capability_iam_users.yaml --capabilities CAPABILITY_NAMED_IAM
+else
+    echo "Updating ${USER_STACK_NAME} stack.."
+    aws cloudformation deploy --stack-name ${USER_STACK_NAME} \
+        --region ${REGION} \
+        --template-file "infrastructure/user-management/iam-users/aws_capability_iam_users.yaml" \
+        --force-upload  \
+        --capabilities "CAPABILITY_NAMED_IAM" \
+        --no-fail-on-empty-changeset
+fi
+
+#Create or update IAM user role stack
+echo "starting ${GROUP_STACK_NAME} stack /create/update"
+if ! aws cloudformation describe-stacks --stack-name ${GROUP_STACK_NAME} --region ${REGION} ; then
+    echo "1"
+    echo "Stack ${GROUP_STACK_NAME} does not exist... Creating the stack..."
+    aws cloudformation create-stack --stack-name ${GROUP_STACK_NAME} \
+        --region ${REGION} \
+        --template-body file://infrastructure/user-management/iam-roles/aws_capability_iam.yaml \
+        --capabilities "CAPABILITY_NAMED_IAM"
+        \
+else
+    echo "Updating ${GROUP_STACK_NAME} stack..."
+    aws cloudformation deploy \
+        --region ${REGION} \
+        --stack-name ${GROUP_STACK_NAME} \
+        --template-file "infrastructure/user-management/iam-roles/aws_capability_iam.yaml" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --no-fail-on-empty-changeset \
+        --force-upload
+fi
+
